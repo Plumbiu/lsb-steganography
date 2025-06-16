3d5663eb5ac79b1f8803aef0b447dd099746b6af
guoxingjun <guoxingjun@meituan.com>
2025-06-16 17:43:19 +0800

    feat: remove sharp

--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,8 @@
 import { writeFile } from 'node:fs/promises'
-import sharp from 'sharp'
+import { Jimp, type JimpInstance } from 'jimp'
 
 interface EncodeOptions {
-  input?: sharp.SharpInput
+  input?: string
 }
 
 const MASK_0F = 0x0f
@@ -11,61 +11,56 @@ const MASK_FF = 0xff
 
 export async function encode(str: string, { input }: EncodeOptions) {
   const buffer = Buffer.from(str)
-  let originImageBuffer: Buffer<ArrayBufferLike> | null = null
+  let image: JimpInstance
   let width = 0
   let height = 0
   if (input) {
-    const imageSharp = sharp(input)
-    const metadata = await imageSharp.metadata()
-    const pixelApset = Math.sqrt(
-      (metadata.width * metadata.height * 3) / (buffer.length * 2),
-    )
-    width = Math.ceil(metadata.width / pixelApset)
-    height = Math.ceil(metadata.height / pixelApset)
-    originImageBuffer = await sharp(input)
-      .resize({ width, height })
-      .raw()
-      .toBuffer()
+    const jimp = await Jimp.read(input)
+    const size = jimp.width * jimp.height * 4
+    const pixelApset = Math.sqrt(size / (buffer.length * 2))
+    width = Math.ceil(jimp.width / pixelApset)
+    height = Math.ceil(jimp.height / pixelApset)
+    // @ts-ignore
+    image = jimp.resize({
+      w: width,
+      h: height,
+    })
   } else {
     const Sqrt3Div2 = Math.sqrt(3 / 2)
     width = Math.ceil(Math.sqrt(buffer.length) / Sqrt3Div2)
     height = width
+    image = new Jimp({
+      color: 0xf0f0f0,
+      width,
+      height,
+    })
   }
 
-  const size = width * height * 3
-  const arrayBuffer = new Uint8Array(size)
-
-  let originIndex = 0
   for (let i = 0; i < buffer.length; i++) {
-    arrayBuffer[i * 2] =
-      ((originImageBuffer?.[originIndex++] ?? MASK_FF) & MASK_F0) +
+    image.bitmap.data[i * 2] =
+      ((image.bitmap.data[i * 2] ?? MASK_FF) & MASK_F0) +
       ((buffer[i] >> 4) & MASK_0F)
-    arrayBuffer[i * 2 + 1] =
-      ((originImageBuffer?.[originIndex++] ?? MASK_FF) & MASK_F0) +
+    image.bitmap.data[i * 2 + 1] =
+      ((image.bitmap.data[i * 2 + 1] ?? MASK_FF) & MASK_F0) +
       (buffer[i] & MASK_0F)
   }
-  for (let i = 0; i < size - buffer.length * 2; i++) {
-    arrayBuffer[buffer.length * 2 + i] =
-      (originImageBuffer?.[originIndex++] ?? MASK_FF) & MASK_F0
+  for (let i = 2 * buffer.length; i < width * height * 4; i++) {
+    image.bitmap.data[i] = (image.bitmap.data[i] ?? MASK_FF) & MASK_F0
   }
-
   return {
-    data: arrayBuffer,
-    writeFile(path: string) {
-      return sharp(arrayBuffer, {
-        raw: { width, height, channels: 3 },
-      }).toFile(path + '.png')
+    data: image.bitmap.data,
+    async writeFile(path: string) {
+      return image.write(`${path}.png`)
     },
   }
 }
 
 export async function decode(imagePath: string) {
-  const buffer = await sharp(imagePath)
-    .raw()
-    .toBuffer({ resolveWithObject: true })
+  const jimpInput = await Jimp.read(imagePath)
+  const buffer = jimpInput.bitmap.data
   let data = ''
-  for (let i = 0; i < buffer.data.length; i++) {
-    data += buffer.data[i].toString(16)[1]
+  for (let i = 0; i < buffer.length; i++) {
+    data += buffer[i].toString(16)[1]
   }
   data = Buffer.from(data, 'hex').toString('utf-8')
   return {
